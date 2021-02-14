68cfcb40e0ba486c4fb1388f4721f5c48b25a82d
ErickElielRojasNavarro <eliel7158@gmail.com>
2021-02-14 10:47:57 -0400

    VERSION DE PRUEBA PARA EVENTOS :D

---
+++ events/events.js
@@ -0,0 +1,15 @@
+var flag=1;
+function t(){
+    if(flag==1){
+    N.style.top="75px";N.style.left="700px"
+    } if(flag==2){
+    N.style.top="115px";N.style.left="100px"
+    }if(flag==3){
+    N.style.top="300px";N.style.left="350px"
+    }flag=flag+1;
+    if(flag==4){
+    flag=1
+    }
+}
+
+function al(){alert("Correcto!")}
